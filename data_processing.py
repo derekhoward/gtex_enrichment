b64a478892eea1312b59c48730303bae3b64a79b
Derek Howard <derekhoward@gmail.com>
2020-09-28 21:19:31 +0000

    fix index

--- data_processing.py
+++ data_processing.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from scipy.stats import zscore
 
-
+print('Loading expression data...')
 metadf = pd.read_table(
     './data/raw/GTEx_v1.9/GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt', sep='\t')
 exp = pd.read_csv('./data/processed/gtex_sample_expression.csv',
@@ -16,7 +16,7 @@ sample_metadata = metadf[metadf.SAMPID.isin(exp.columns)].loc[:, [
     'SAMPID', 'SMTS', 'SMTSD', 'SMUBRID']]
 sample_metadata['donor_id'] = sample_metadata.SAMPID.str.split(
     '-').str[:2].str.join('-')
-
+print('Merge expression with metadata')
 merged_data = exp.T.merge(sample_metadata.loc[:, [
     'SAMPID', 'donor_id', 'SMTSD']], left_index=True, right_on='SAMPID')
 merged_data = merged_data.set_index('SAMPID')
@@ -35,6 +35,7 @@ def process_donor_subset(subset_df):
     return processed
 
 
+print('Processing donors individually')
 transformed_donor_dfs = []
 for donor in donors:
     subset = merged_data[merged_data.donor_id == donor]
@@ -44,6 +45,6 @@ for donor in donors:
 all_donors = pd.concat(transformed_donor_dfs)
 tissue_level_mean = all_donors.drop('donor_id', axis=1).groupby('SMTSD').mean()
 tissue_level_mean.to_csv('./data/processed/gtex_processed.csv')
-tissue_level_ranks = tissue_level_mean.set_index('SMTSD').T.rank()
+tissue_level_ranks = tissue_level_mean.T.rank()
 tissue_level_ranks.index.name = 'gene_symbol'
 tissue_level_ranks.to_csv('./data/processed/gtex_processed_ranks.csv')
